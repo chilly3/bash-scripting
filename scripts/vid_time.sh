3ed57a1944494907d8d9e2f34e72587f47f115af
chilly3 <chilly3@tutanota.com>
2022-03-10 16:01:12 -0600

    Updated vid_time correcting octal error and base 10 printing plus text files for each section of video list

--- scripts/vid_time.sh
+++ scripts/vid_time.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #script for parsing text document for video length times from vid_list.txt (total time for compTIA course sec 3.3-3.11) Copied from website
-#Script needs to be updated to handle invalid user input
 
 IFS=$'\n'
 
@@ -22,26 +21,31 @@ echo "Input selected: "${vid_pick}
 #parse file into array of numbers to be added
 v_list=($(cat ${vid_pick} | awk -e '/\)$/ {print $NF}' | awk '{gsub(/\(/,"");gsub(/\)/,"");gsub(/:/," "); print $0}'))
 
+#declare -p v_list
+
 #time variables
 v_mins=0
 v_secs=0
 t_time=0
 
 
-#iterate list adding column 1 (minutes) and 2 (seconds) separately
+#iterate list adding column 1 (minutes) and 2 (seconds) separately (10# base 10 added to fix error caused by leading zero causing number to be interpreted as octal)
 for i in "${v_list[@]}"
 do
   let v_mins=${v_mins}+$(echo ${i} | cut -f1 -d" ")
-  let v_secs=${v_secs}+$(echo ${i} | cut -f2 -d" ")
+  let v_secs=${v_secs}+10#$(echo ${i} | cut -f2 -d" ")
+  #printf ${v_secs}" "
 done
 
 #echo ${v_mins}
 #echo ${v_secs}
 
-#calculate minutes and remaining seconds from total seconds
+#calculate minutes and remaining seconds from total seconds (printf call added to print single digit number with zero preceding it for results ofseconds less than 10
 let sec_rem=${v_secs}%60
+printf -v sec_rem "%02d\n" ${sec_rem}
+
 let sec_min=${v_secs}/60
 
 let v_mins=${v_mins}+${sec_min}
 
-echo "Total Time: "${v_mins}":"${sec_rem}
+echo "Total Time: ${v_mins}:${sec_rem}"
